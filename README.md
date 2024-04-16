3b513d4f8c3fe9d11b837244fdad927f0133bb6a
Thorsten Kattanek <thorsten.kattanek@gmx.de>
2024-04-16 18:56:43 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# c_sharp_demo
+ test_sdl2_dotnet
---
+++ c_sharp_demo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using SDL2;
+using SDL2.Bindings;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+SDL.SDL_Init(SDL.SDL_INIT_VIDEO);
+
+var window = SDL.SDL_CreateWindow("DEMO",
+	SDL.SDL_WINDOWPOS_CENTERED,
+	SDL.SDL_WINDOWPOS_CENTERED,
+	800,
+	600,
+	SDL.SDL_WindowFlags.SDL_WINDOW_SHOWN
+	);
+
+	while(true);
